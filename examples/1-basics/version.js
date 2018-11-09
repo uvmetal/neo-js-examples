a1ca8c3c01a857c2b4dc889558e4d54b22f886dc
Travis <travis@localhost.dev>
2018-11-09 22:09:39 +1100

    update dependency to correct namespace, and to 0.10.0-rc.2

--- examples/1-basics/version.js
+++ examples/1-basics/version.js
@@ -1,10 +1,20 @@
-const Neo = require('neo-js').Neo
+/**
+ * Fetching version information of neo-js.
+ */
+const Neo = require('@cityofzion/neo-js').Neo
 
 // -- Implementation
 
 ;(async () => {
   console.log('== Get Version Example ==')
+
   console.log('Neo class version:', Neo.VERSION)
+  // <example response>
+  // > Neo class version: 0.10.0-rc.1
+
   console.log('Neo class user agent:', Neo.UserAgent)
+  // <example response>
+  // > Neo class user agent: NEO-JS:0.10.0-rc.1
+
   console.log('== THE END ==')
 })()
