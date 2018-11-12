54ebfd3abe7f45257a2db2e237a3d3534d53000f
Travis <travis@localhost.dev>
2018-11-12 11:22:13 +1100

    Sync example to pass in own endpoint list

--- examples/sync/sync-mainnet.js
+++ examples/sync/sync-mainnet.js
@@ -12,6 +12,60 @@ process.on('unhandledRejection', (reason, promise) => {
 // -- Parameters
 
 const network = 'mainnet'
+const endpoints = [
+  { endpoint: 'https://seed1.switcheo.network:10331' },
+  { endpoint: 'https://seed2.switcheo.network:10331' },
+  { endpoint: 'https://seed3.switcheo.network:10331' },
+  { endpoint: 'https://seed4.switcheo.network:10331' },
+  { endpoint: 'https://seed5.switcheo.network:10331' },
+  { endpoint: 'http://seed1.travala.com:10332' },
+  { endpoint: 'https://seed1.neo.org:10331' },
+  { endpoint: 'http://seed2.neo.org' },
+  { endpoint: 'http://seed3.neo.org' },
+  { endpoint: 'http://seed4.neo.org' },
+  { endpoint: 'http://seed5.neo.org' },
+  { endpoint: 'http://api.otcgo.cn' },
+  { endpoint: 'https://seed1.cityofzion.io:443' },
+  { endpoint: 'https://seed2.cityofzion.io:443' },
+  { endpoint: 'https://seed3.cityofzion.io:443' },
+  { endpoint: 'https://seed4.cityofzion.io:443' },
+  { endpoint: 'https://seed5.cityofzion.io:443' },
+  { endpoint: 'https://seed0.cityofzion.io:443' },
+  { endpoint: 'https://seed6.cityofzion.io:443' },
+  { endpoint: 'https://seed7.cityofzion.io:443' },
+  { endpoint: 'https://seed8.cityofzion.io:443' },
+  { endpoint: 'https://seed9.cityofzion.io:443' },
+  { endpoint: 'http://node1.ams2.bridgeprotocol.io' },
+  { endpoint: 'http://node2.ams2.bridgeprotocol.io' },
+  { endpoint: 'http://node1.nyc3.bridgeprotocol.io' },
+  { endpoint: 'http://node2.nyc3.bridgeprotocol.io' },
+  { endpoint: 'http://node1.sgp1.bridgeprotocol.io' },
+  { endpoint: 'http://node2.sgp1.bridgeprotocol.io' },
+  { endpoint: 'https://seed1.redpulse.com:443' },
+  { endpoint: 'https://seed2.redpulse.com:443' },
+  { endpoint: 'http://seed1.aphelion-neo.com:10332' },
+  { endpoint: 'http://seed2.aphelion-neo.com:10332' },
+  { endpoint: 'http://seed3.aphelion-neo.com:10332' },
+  { endpoint: 'http://seed4.aphelion-neo.com:10332' },
+  { endpoint: 'https://seed1.spotcoin.com:10332' },
+  { endpoint: 'http://rustylogic.ddns.net:10332' },
+  { endpoint: 'http://seed1.ngd.network:10332' },
+  { endpoint: 'http://seed2.ngd.network:10332' },
+  { endpoint: 'http://seed3.ngd.network:10332' },
+  { endpoint: 'http://seed4.ngd.network:10332' },
+  { endpoint: 'http://seed5.ngd.network:10332' },
+  { endpoint: 'http://seed6.ngd.network:10332' },
+  { endpoint: 'http://seed7.ngd.network:10332' },
+  { endpoint: 'http://seed8.ngd.network:10332' },
+  { endpoint: 'http://seed9.ngd.network:10332' },
+  { endpoint: 'http://seed10.ngd.network:10332' },
+  { endpoint: 'https://seed1.red4sec.com:10332' },
+  { endpoint: 'http://seed.neoeconomy.io:10332' },
+  { endpoint: 'http://node1.plutolo.gy:10332' },
+  { endpoint: 'http://seed1.cryptoholics.cc:10332' },
+  { endpoint: 'http://neo-node.com:10332' },
+]
+const storageType = 'mongodb'
 const dbConnectionString = 'mongodb://localhost/neo_mainnet'
 const blockCollectionName = 'blocks'
 
@@ -21,8 +75,9 @@ const blockCollectionName = 'blocks'
   console.log('== Sync Mainnet ==')
 
   const neo = new Neo({
-    network: network,
-    storageType: 'mongodb',
+    network,
+    endpoints,
+    storageType,
     storageOptions: {
       connectionString: dbConnectionString,
       collectionNames: {
